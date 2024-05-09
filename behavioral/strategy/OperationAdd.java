49e5326f8ab7c6db5506020a0327e9032bf2d08f
bethmij <bethmij@gmail.com>
2024-05-09 11:57:50 +0530

    implement strategy

---
+++ behavioral/strategy/OperationAdd.java
@@ -0,0 +1,7 @@
+public class OperationAdd implements Strategy{
+    @Override
+    public int doOperator(int num1, int num2) {
+        return num1+num2;
+    }
+
+}
---
+++ behavioral/strategy/OperationSubtraction.java
@@ -0,0 +1,6 @@
+public class OperationSubtraction implements Strategy {
+    @Override
+    public int doOperator(int num1, int num2) {
+        return num1-num2;
+    }
+}
