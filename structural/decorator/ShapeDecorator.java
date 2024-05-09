ad5ee434519f19bca694da7bd4c685a16ece144d
bethmij <bethmij@gmail.com>
2024-05-09 11:24:58 +0530

    implement decorator

---
+++ structural/decorator/ShapeDecorator.java
@@ -0,0 +1,13 @@
+public abstract class ShapeDecorator implements Shape{
+
+    protected Shape decoratorShape;
+
+    public ShapeDecorator (Shape decoratorShape){
+        this.decoratorShape = decoratorShape;
+    }
+
+    @Override
+    public void draw() {
+        decoratorShape.draw();
+    }
+}
