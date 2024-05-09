2ce84460593da8f14d4dbff248f614b305f320c1
bethmij <bethmij@gmail.com>
2024-05-09 09:57:19 +0530

    implement singleton

---
+++ creational/singleton/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SingletonDemo singletonDemo = SingletonDemo.getSingletonDemo();
+        System.out.println(singletonDemo);
+    }
+
+}
